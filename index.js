af0a8da36776fcddf65364ec899f9bad5bf5e180
Shuo Tan <ts@vsoft.io>
2019-06-12 16:10:45 +0800

    make compatible with aws-china
    Signed-off-by: Shuo Tan <ts@vsoft.io>

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ class CustomRoles {
           'Fn::Join': [
             ':',
             [
-              'arn:aws:logs',
+              'arn',
+              { Ref: 'AWS::Partition' },
+              'logs',
               { Ref: 'AWS::Region' },
               { Ref: 'AWS::AccountId' },
               `log-group:/aws/lambda/${functionName}:*`
@@ -59,7 +61,9 @@ class CustomRoles {
           'Fn::Join': [
             ':',
             [
-              'arn:aws:logs',
+              'arn',
+              { Ref: 'AWS::Partition' },
+              'logs',
               { Ref: 'AWS::Region' },
               { Ref: 'AWS::AccountId' },
               `log-group:/aws/lambda/${functionName}:*:*`
